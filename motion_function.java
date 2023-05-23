4f0ccf67b7a07c8f07f0a2e6570280121d48e0b6
somedude000 <131830685+somedude000@users.noreply.github.com>
2023-05-23 12:10:18 +0800

    Create motion_function.java
    created function file for the motion calculations.

---
+++ motion_function.java
@@ -0,0 +1,15 @@
+public class motion_function
+{
+    private double velocity;
+    private double displacment;
+    private double acceleration;
+    private double time;
+
+    public Main_function(){
+        velocity = 0;
+        displacment = 0;
+        acceleration = 0;
+        time = 0;
+    }
+
+}
